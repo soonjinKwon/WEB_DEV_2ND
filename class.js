b75cdc057a42c14d1026992ff38c2a34268a122e
Kwon <sjkwon6653@gmail.com>
2020-04-05 15:15:32 +0900

    상속에서 super 사용

--- class.js
+++ class.js
@@ -7,18 +7,24 @@ class Person{
 		this.third = third;
 	}
 	sum(){
-		return 'prototype:'+ (this.first + this.second + this.third);
+		return this.first + this.second + this.third;
 	}
 }   
 
 class PersonPlus extends Person{
+	constructor(name, first, second, third, forth){
+		super(name, first, second, third);
+		this.forth = forth;
+	}
+	sum(){
+		return super.sum() + this.forth;
+	}
 	avg(){
-		return (this.first + this.second + this.third)/3;
+		return (this.first + this.second + this.third + this.forth)/4; 
 	}
 }   
 
-
-var kim = new PersonPlus('kim', 10, 20, 50);
+var kim =new PersonPlus('kim', 10, 20, 50, 70);
 
 console.log('kim.sum()', kim.sum());
 console.log('kim.avg()', kim.avg());
