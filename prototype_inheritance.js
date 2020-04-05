0babfebe7d65acc9e5594d1014132032778d20d6
Kwon <sjkwon6653@gmail.com>
2020-04-05 16:16:49 +0900

    __proto__ 사용

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -1,9 +1,10 @@
 var superObj = {superVal : 'super'}
-var subObj = {subVal : 'sub'}
-
-subObj.__proto__ = superObj;
+// var subObj = {subVal : 'sub'}
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+debugger;
 console.log('subObj.subVal =>', subObj.subVal);
 console.log('superObj.superVal =>', subObj.superVal);
-
-subObj.superVal = 'Sub';
+subObj.superVal = 'sub';
 console.log('subObj.superVal =>', subObj.superVal);
