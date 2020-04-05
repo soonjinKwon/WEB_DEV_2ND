4e2f45f50c2ed4ba7352e12664307b57867ca863
Kwon <sjkwon6653@gmail.com>
2020-04-05 13:45:43 +0900

    class 셈플

---
+++ class.js
@@ -0,0 +1,14 @@
+
+class Person{
+	constructor(name, first, second, third){
+		this.name = name;
+		this.first = first; 
+		this.second = second;
+		this.third = third;
+	}
+}  
+
+
+var kim = new Person('kim', 10, 20, 50);
+console.log('kim', kim);
+
