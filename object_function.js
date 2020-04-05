dea54eabf8c9e46b67d3c0036e3b6f1a60489d72
Kwon <sjkwon6653@gmail.com>
2020-04-05 17:04:05 +0900

    bind 함수적용

--- object_function.js
+++ object_function.js
@@ -1,9 +1,14 @@
 var kim = {name: 'kim', first:20, second:30}
 var lee = {name: 'lee', first:10, second:20}
-function sum(){
-	return this.first + this.second;
+function sum(prefix){
+	return prefix + (this.first + this.second);
 }
 
 console.log('sum.call(kim)', sum.call(kim));
+
+
+var kimSum = sum.bind(kim, '->');
+console.log('kimSum()', kimSum());
+
 
 
