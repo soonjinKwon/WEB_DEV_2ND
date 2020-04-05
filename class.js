4e741ad1f3165d3febdbee40a83632b22221478f
Kwon <sjkwon6653@gmail.com>
2020-04-05 14:39:24 +0900

    class 메소드 추가시 중복삭제 = > 상속기능사용

--- class.js
+++ class.js
@@ -11,16 +11,7 @@ class Person{
 	}
 }   
 
-class PersonPlus{
-	constructor(name, first, second, third){
-		this.name = name;
-		this.first = first; 
-		this.second = second;
-		this.third = third;
-	}
-	sum(){
-		return 'prototype:'+ (this.first + this.second + this.third);
-	}
+class PersonPlus extends Person{
 	avg(){
 		return (this.first + this.second + this.third)/3;
 	}
@@ -32,3 +23,4 @@ var kim = new PersonPlus('kim', 10, 20, 50);
 console.log('kim.sum()', kim.sum());
 console.log('kim.avg()', kim.avg());
 
+ 
